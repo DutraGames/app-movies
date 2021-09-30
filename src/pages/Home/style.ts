d22a4fe46ccf37ec820f0257ec37b1b04dcb23e7
DutraGames <gabrieldutraamarante@gmail.com>
2021-09-30 00:52:53 -0300

    Styles Banner e Seach Area

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -5,3 +5,50 @@ background-color: #090011;
 padding: 4px 0;
 flex:  1;
 `
+
+export const AreaSeach = styled.View`
+flex-direction: row;
+display: flex;
+width: 100%;
+height: 40px;
+`
+
+export const SeachButton = styled.TouchableOpacity`
+width: 12%;
+height: 100%;
+align-items: center;
+justify-content: center;
+`
+
+export const SeachCampo = styled.TextInput`
+width: 83%;
+height: 100%;
+background-color: #505050;
+color: #fff;
+padding: 0 10px;
+margin-left: 15px;
+border-radius: 8px;
+font-size: 15px;
+`
+
+
+export const Title = styled.Text`
+margin-left: 15px;
+color: #fff;
+font-size: 30px;
+font-weight: bold;
+`
+
+export const BannerButton = styled.TouchableOpacity`
+width: 100%;
+padding: 15px;
+height: 230px;
+`
+
+export const Banner = styled.Image`
+height: 100%;
+width: 100%;
+border-radius: 10px;
+`
+
+import {} from 'react-native'
