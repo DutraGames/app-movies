03497c1b293985025d0eb310bb769a0e80756435
DutraGames <gabrieldutraamarante@gmail.com>
2021-09-30 00:52:05 -0300

    Seach Area e Banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,26 @@
 import React from "react"
-
-import {View, Text} from 'react-native'
-
 import Header from "../../components/Header"
-import {Container} from './style'
+import {Container, AreaSeach, SeachButton, SeachCampo, Title, Banner, BannerButton} from './style'
+
+import {Feather} from '@expo/vector-icons'
 
 export default function Home(){
     return(
        <Container>
            <Header title="Início"/>
+
+           <AreaSeach>
+                <SeachCampo  placeholder="Vingadores" placeholderTextColor="#888"/>
+                <SeachButton>
+                    <Feather name="search" size={35} color="#fff" />
+                </SeachButton>
+           </AreaSeach>
+
+           <BannerButton>
+               <Banner resizeMethod="resize" source={{uri: 'https://f.i.uol.com.br/fotografia/2019/04/23/15560338625cbf314652022_1556033862_3x2_md.jpg'}}/>
+           </BannerButton>
+
+           <Title>Lançamentos</Title>
        </Container>
     )
 }
