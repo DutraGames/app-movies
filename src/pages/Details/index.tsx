3508985d29f494c297d9b7f309b159fae8523342
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-02 23:25:40 -0300

    Page Details create

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,86 @@
+import React, {useEffect, useState} from "react"
+import { Container, Title,Header, ButtonHeader, Banner, ButtonLink, RateArea, Rate, ListGenre, Des} from "./style"
+import {Feather, Ionicons} from '@expo/vector-icons'
+import Stars from 'react-native-stars'
+
+import {useNavigation, useRoute} from '@react-navigation/native'
+import api, {key} from "../../api"
+import Genre from '../../components/Genre'
+import { ScrollView } from "react-native-gesture-handler"
+
+export default function Details(){
+
+    const Navigation = useNavigation()
+    const Route = useRoute()
+
+    const [movie, setMovie] = useState<object>({})
+
+    useEffect(() => {
+        let isActive:boolean = true
+
+        async function getMovie() {
+            const response = await api.get(`/movie/${Route.params?.id}`, {
+                params:{
+                    api_key: key,
+                    language: 'pt-BR',
+                }
+            })
+
+            if(isActive){
+                setMovie(response.data)
+            }
+        }
+
+        getMovie()
+
+        return () =>{
+            isActive = false
+        }
+    }, [])
+    
+    return(
+        <Container>
+            <Header>
+                <ButtonHeader onPress={() => Navigation.goBack()}>
+                <Feather name="arrow-left" size={28} color="#fff" />
+                </ButtonHeader>
+
+                <ButtonHeader>
+                <Feather name="bookmark" size={28} color="#fff" />
+                </ButtonHeader>
+            </Header>
+            <Banner resizeMethod="resize" source={{uri: `https://image.tmdb.org/t/p/original/${movie.poster_path}`}}/>
+
+            <ButtonLink>
+                <Feather name="link" size={35} color="#fff" />
+            </ButtonLink>
+
+            <Title numberOfLines={2}>{movie.title}</Title>
+
+            <RateArea>
+                <Stars half={true}
+                default={movie.vote_average}
+                count={10}
+                starSize={20}
+                fullStar={<Ionicons name="star" size={24} color="#eaa33e" />}
+                emptyStar={<Ionicons name="star-outline" size={24} color="#eaa33e" />}
+                halfStar={<Ionicons name="star-half" size={24} color="#eaa33e" />}
+                disabled={true}
+                />
+                <Rate>{movie.vote_average}/10</Rate>
+            </RateArea>
+
+            <ListGenre data={movie?.genres}
+            renderItem={({item}:any) => <Genre data={item}/>}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false} 
+            keyExtractor={(item:any) => String(item.id)}
+            />
+
+            <Title>Descrição</Title>
+            <ScrollView>
+                <Des>{movie.overview}</Des>
+            </ScrollView>
+        </Container>
+    )
+}
---
+++ src/pages/Details/style.ts
@@ -0,0 +1,84 @@
+import Styled from 'styled-components/native/'
+
+export const Container = Styled.View`
+background-color: #090011;
+padding: 4px 0;
+flex:  1;
+`
+
+export const Header = Styled.View`
+z-index: 2;
+flex-direction: row;
+justify-content: space-between;
+padding: 0 15px;
+position: absolute;
+top: 20px;
+display: flex;
+width: 100%;
+
+`
+
+export const Title = Styled.Text`
+font-size: 22px;
+padding-left: 15px;
+font-weight: bold;
+color: #fff;
+margin-top: 10px;
+`
+
+export const ButtonHeader = Styled.TouchableOpacity`
+background-color: #090015;
+width: 46px;
+height: 46px;
+border-radius: 23px;
+justify-content: center;
+align-items: center;
+`
+
+export const ButtonLink = Styled.TouchableOpacity`
+background-color: #f00;
+width: 60px;
+height: 60px;
+align-items: center;
+justify-content: center;
+border-radius: 30px;
+position: absolute;
+z-index: 2;
+top: 320px;
+right: 30px;
+`
+
+export const Banner = Styled.Image`
+width: 100%;
+height: 350px;
+border-bottom-right-radius: 70px;
+border-bottom-left-radius: 70px;
+`
+
+export const RateArea = Styled.View`
+width: 100%;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+padding: 0 15px;
+`
+
+export const Rate = Styled.Text`
+color: #FFF;
+font-size: 25px;
+font-weight: bold;
+`
+
+export const ListGenre = Styled.FlatList`
+max-height: 35px;
+padding-left: 15px;
+margin: 8px 0;
+`
+
+export const Des = Styled.Text`
+color: #fff;
+padding: 0 15px;
+padding-bottom: 10px;
+line-height: 25px;
+font-size: 18px;
+`
