1c72cfd1857d17c230006698dc6ef2e87b804ec7
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-02 22:07:49 -0300

    Add a rota detalhes

--- src/router/stack.tsx
+++ src/router/stack.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 import Home from "../pages/Home";
+import Details from "../pages/Details";
 
 const Stack = createNativeStackNavigator()
 
@@ -9,6 +10,7 @@ function StackRouter() {
     return (
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+        <Stack.Screen name="Detalhes" component={Details} options={{headerShown: false}} />
       </Stack.Navigator>
     );
   }
