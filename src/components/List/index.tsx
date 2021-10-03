90072ef2d44f508ae2ab14e33824ea4e44990a15
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-02 22:08:51 -0300

    bug corrigido

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import {Container, Title, Banner, Rate, RateArea} from './style'
 import { Ionicons } from '@expo/vector-icons';
 
-export default function List({data}:any){
+export default function List({data, NavigatorPage}:any){
     return(
-        <Container>
+        <Container onPress={() => NavigatorPage(data)}>
             <Banner resizeMethod="resize" source={{uri: `https://image.tmdb.org/t/p/original/${data.poster_path}`}}/>
             <Title numberOfLines={1}>{data.title}</Title>
             <RateArea>
