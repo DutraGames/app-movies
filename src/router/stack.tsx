f198e2fecb69873c588611cdc6329e6e57b5531a
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-04 23:56:00 -0300

    add page Seach in Router

--- src/router/stack.tsx
+++ src/router/stack.tsx
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 import Home from "../pages/Home";
 import Details from "../pages/Details";
+import Search from "../pages/Search";
 
 const Stack = createNativeStackNavigator()
 
@@ -11,6 +12,15 @@ function StackRouter() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
         <Stack.Screen name="Detalhes" component={Details} options={{headerShown: false}} />
+        <Stack.Screen name="Busca" component={Search} options={{title: 'Pesquisa',
+      headerTintColor: '#fff',
+      headerTitleStyle:{
+        color: '#fff',
+      },
+      headerStyle:{
+        backgroundColor: '#090011'
+      }
+      }} />
       </Stack.Navigator>
     );
   }
