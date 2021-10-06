90daee0eceb8c6db686d32a8ba4ffcf5af2d70fd
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-06 15:57:44 -0300

    item delete e buscando item

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -13,7 +13,7 @@ export async function MoviesSave(key:string, movie) {
 
     const hasmovie = movieStored.some(item => item.id === movie.id)
 
-    if(hasmovie.id){
+    if(hasmovie){
         console.log("existe já")
         return
     }
@@ -24,4 +24,26 @@ export async function MoviesSave(key:string, movie) {
     console.log("Salvou")
 }
 
+export async function MoviesDelete(id) {
+    let movieStored = await GetMoviesSave('@favorites')
+    let myMovies = movieStored.filter(item => {
+        return (item.id !== id)
+    })
+
+    await AsyncStorage.setItem('@favorites', JSON.stringify(myMovies))
+    console.log("Delete")
+    return myMovies
+}
+
+export async function MoviesHas(movie) {
+    let movieStored = await GetMoviesSave('@favorites')
+
+    const hasMovie = movieStored.find(item => item.id === movie.id)
+
+    if(hasMovie){
+        return true
+    }
+
+    return false
+}
 
