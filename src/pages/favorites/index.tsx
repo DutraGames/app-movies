63cbb369725224c708fccba972c0b1fc5358aba9
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-07 15:45:17 -0300

    create design Favorites Page

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,11 +1,45 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import { FlatList } from "react-native-gesture-handler"
 
-import {View, Text} from 'react-native'
+import Header from '../../components/Header'
+import {Container} from './style'
+import {GetMoviesSave} from '../../util/storage'
+import FavoriteItem from "../../components/FavoritesItem"
 
 export default function Favorites(){
+
+    const [movies, setMovies] = useState<object>([])
+
+    useEffect(() => {
+        let isActive = true
+
+        async function getFavoritesMovies() {
+            const results =  await GetMoviesSave('@favorites')
+
+            if (isActive) {
+                setMovies(results)
+            }
+        }
+
+        if (isActive) {
+            getFavoritesMovies()
+        }
+
+        return () => {
+            isActive = false
+        }
+    }, [])
+
+
     return(
-        <View>
-            <Text>Favorite</Text>
-        </View>
+        <Container>
+            <Header title="Favoritos"/>
+
+            <FlatList data={movies}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={item => String(item.id)}
+                renderItem={({item}) => <FavoriteItem data={item}/>}
+            />
+        </Container>
     )
 }
---
+++ src/pages/favorites/style.ts
@@ -0,0 +1,7 @@
+import Styled from 'styled-components/native/'
+
+export const Container = Styled.View`
+background-color: #090011;
+padding: 4px 0;
+flex:  1;
+`
