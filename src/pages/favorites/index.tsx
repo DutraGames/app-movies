11f2f8a64831d2b92a5a28ed73b55766a37a16a5
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-07 20:28:40 -0300

    add funções de navigate e Delete

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -3,12 +3,15 @@ import { FlatList } from "react-native-gesture-handler"
 
 import Header from '../../components/Header'
 import {Container} from './style'
-import {GetMoviesSave} from '../../util/storage'
+import {GetMoviesSave, MoviesDelete} from '../../util/storage'
 import FavoriteItem from "../../components/FavoritesItem"
+import {useNavigation, useIsFocused} from '@react-navigation/native'
 
 export default function Favorites(){
 
     const [movies, setMovies] = useState<object>([])
+    const Navigate = useNavigation()
+    const isFocused = useIsFocused()
 
     useEffect(() => {
         let isActive = true
@@ -28,8 +31,16 @@ export default function Favorites(){
         return () => {
             isActive = false
         }
-    }, [])
+    }, [isFocused])
 
+    const hasDelete = async(id) =>{
+        const results = await MoviesDelete(id)
+        setMovies(results)
+    }
+
+    const DetailPage = (item) =>{
+        Navigate.navigate("Detalhes", {id: item.id})
+    }
 
     return(
         <Container>
@@ -38,7 +49,11 @@ export default function Favorites(){
             <FlatList data={movies}
             showsVerticalScrollIndicator={false}
             keyExtractor={item => String(item.id)}
-                renderItem={({item}) => <FavoriteItem data={item}/>}
+                renderItem={({item}) => <FavoriteItem 
+                data={item}
+                remove={hasDelete}
+                navigate={() => DetailPage(item)}
+                />}
             />
         </Container>
     )
