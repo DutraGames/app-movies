2d4c0f95526eaa65eb9028e4528da50e091909b3
DutraGames <gabrieldutraamarante@gmail.com>
2021-10-07 22:51:03 -0300

    Page Abou começando

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,11 +1,44 @@
-import React from "react"
-
-import {View, Text} from 'react-native'
+import React, {useEffect, useState} from "react"
+import {Container, ButtonGit} from './style'
+import Header from "../../components/Header"
+import { AntDesign } from '@expo/vector-icons';
+import {WebView} from 'react-native-webview'
+import {Modal} from 'react-native'
 
 export default function About(){
+
+    const [dados, setDados] = useState<object>({})
+
+    useEffect(() => {
+        let isActive = true
+
+        async function getGithub() {
+            const res = await fetch('https://api-dutra.herokuapp.com/github')
+            const data = await res.json()
+
+            if (isActive) {
+                setDados(data)
+            }
+        }
+
+        if (isActive) {
+            getGithub()
+        }
+
+
+        return () => {
+            isActive = false
+        }
+    }, [])
+
+
     return(
-        <View>
-            <Text>Sobre</Text>
-        </View>
+        <Container>
+            <Header title="Sobre"/>
+            
+            <ButtonGit>
+                <AntDesign name="github" size={50} color="#fff"/>
+            </ButtonGit>
+        </Container>
     )
 }
---
+++ src/pages/About/style.ts
@@ -0,0 +1,18 @@
+import Styled from 'styled-components/native'
+
+export const Container = Styled.View`
+flex: 1;
+background-color: #090011;
+padding: 4px 0;
+`
+
+export const Name = Styled.Text`
+font-size: 20px;
+color: #fff;
+font-weight: bold;
+`
+
+export const ButtonGit = Styled.TouchableOpacity`
+align-items: center;
+justify-content: center;
+`
